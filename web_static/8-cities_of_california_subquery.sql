e9780fe18668b526da70401be26f6b76046d9ea5
Yonas Kenenisa <48440474+yonasTK@users.noreply.github.com>
2023-01-10 11:04:59 +0300

    Delete web_static directory

--- web_static/8-cities_of_california_subquery.sql
+++
@@ -1,5 +0,0 @@
--- lists all the cities of California
-
-SELECT id, name FROM cities
-WHERE state_id = (SELECT id FROM states
-	WHERE name = "California");
--- web_static/9-cities_by_state_join.sql
+++
@@ -1,6 +0,0 @@
--- lists all cities in db
-
-SELECT cities.id, cities.name, states.name
-FROM cities
-INNER JOIN states
-WHERE cities.state_id = states.id;
